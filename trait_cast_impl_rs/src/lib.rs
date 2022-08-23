2bf4f3cdaa6e9e9172de580c9fce687ba9e1e41e
onestacked <chrisi.schrefl@gmail.com>
2022-08-23 11:14:29 +0200

    Use decl macro invocation in proc macro

--- trait_cast_impl_rs/src/lib.rs
+++ trait_cast_impl_rs/src/lib.rs
@@ -4,230 +4,11 @@
 //!
 //! See `make_trait_castable` for more details.
 
-use std::{
-  fmt::{self, Display, Formatter},
-  iter::Peekable,
-};
-
 use proc_macro::TokenStream as TokenStream1;
-use proc_macro2::{Ident, Span, TokenStream, TokenTree};
-use quote::{quote, quote_spanned, ToTokens, TokenStreamExt};
+use proc_macro2::TokenStream;
+use quote::quote;
 use venial::{parse_declaration, Declaration, Error};
 
-struct Type {
-  fully_qualified: bool, // if it starts with ::
-  path: Vec<Ident>,
-  generics: Vec<Type>,
-}
-impl Type {
-  fn is_punct(tt: &TokenTree, c: char) -> bool {
-    if let TokenTree::Punct(p) = tt && p.as_char() == c {true} else {false}
-  }
-  fn is_some_punct(tt: Option<&TokenTree>, c: char) -> bool {
-    if let Some(TokenTree::Punct(p)) = tt && p.as_char() == c {true} else {false}
-  }
-  fn expect_punct(tt: &TokenTree, c: char) -> Result<(), Error> {
-    Self::is_punct(tt, c)
-      .then_some(())
-      .ok_or_else(|| Error::new_at_tokens(&tt, format!("Expected '{}' but found: '{}'", c, tt)))
-  }
-  fn take_path_sep(input: &mut Peekable<impl Iterator<Item = TokenTree>>) -> Result<bool, Error> {
-    if Self::is_some_punct(input.peek(), ':') {
-      let last = input.next();
-      let next = input.next();
-      if let Some(next) = next {
-        Self::expect_punct(&next, ':')?;
-        Ok(true)
-      } else {
-        Err(Error::new_at_tokens(last, "Expected ':' but got to end"))
-      }
-    } else {
-      Ok(false)
-    }
-  }
-  fn as_ident(input: Option<TokenTree>) -> Result<Ident, Error> {
-    input.map_or_else(
-      || Err(Error::new("Expected Identifier")),
-      |input| {
-        if let TokenTree::Ident(i) = input {
-          Ok(i)
-        } else {
-          Err(Error::new_at_tokens(
-            &input,
-            format!("Expected Identifier but found: '{}'", input),
-          ))
-        }
-      },
-    )
-  }
-  fn parse(input: &mut Peekable<impl Iterator<Item = TokenTree>>) -> Result<Self, Error> {
-    let fully_qualified = Self::take_path_sep(input)?;
-
-    let mut ty = Self {
-      fully_qualified,
-      path: vec![Self::as_ident(input.next())?],
-      generics: vec![],
-    };
-
-    while let Some(tt) = input.peek() {
-      let err_msg = Error::new_at_tokens(tt, format!("Expected ::, <, > or , but found: '{}'", tt));
-      if Self::is_punct(tt, '<') {
-        input.next();
-        ty.generics = Self::parse_vec_generics(input)?;
-        if let Some(tt) = input.peek() {
-          if Self::is_punct(tt, '>') {
-          } else if Self::is_punct(tt, ',') {
-            input.next();
-          } else {
-            unreachable!("Got {} at unexpected place", tt) // i think, otherwise give some error
-          }
-        }
-        break;
-      } else if Self::is_punct(tt, '>') {
-        break;
-      } else if Self::is_punct(tt, ',') {
-        input.next();
-        break;
-      } else if Self::take_path_sep(input)? {
-        ty.path.push(Self::as_ident(input.next())?);
-      } else {
-        return Err(err_msg);
-      }
-    }
-    Ok(ty)
-  }
-  fn parse_vec(mut input: Peekable<impl Iterator<Item = TokenTree>>) -> Result<Vec<Self>, Error> {
-    let ret = Self::parse_vec_inner(&mut input)?;
-    input.next().map_or(Ok(ret), |tt| {
-      Err(Error::new_at_tokens(
-        &tt,
-        format!("Unconsumed Input '{}'", tt),
-      ))
-    })
-  }
-
-  fn parse_vec_generics(
-    input: &mut Peekable<impl Iterator<Item = TokenTree>>,
-  ) -> Result<Vec<Self>, Error> {
-    let ret = Self::parse_vec_inner(input)?;
-    if let Some(end) = input.next() {
-      Self::expect_punct(&end, '>')?;
-      Ok(ret)
-    } else {
-      Err(Error::new("Expected '>' but got to end"))
-    }
-  }
-  fn parse_vec_inner(
-    input: &mut Peekable<impl Iterator<Item = TokenTree>>,
-  ) -> Result<Vec<Self>, Error> {
-    let mut out = vec![];
-    while let Some(tt) = input.peek() {
-      if Self::is_punct(tt, '>') {
-        return Ok(out);
-      }
-      out.push(Self::parse(input)?);
-    }
-    Ok(out)
-  }
-  fn span(&self) -> Span {
-    self.path[self.path.len() - 1].span()
-  }
-}
-impl Display for Type {
-  fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), fmt::Error> {
-    if self.fully_qualified {
-      write!(f, "::")?;
-    }
-    write!(f, "{}", self.path[0])?;
-    for part in &self.path[1..] {
-      write!(f, "::{}", part)?;
-    }
-    if !self.generics.is_empty() {
-      write!(f, "<{}", self.generics[0])?;
-      for generic in &self.generics[1..] {
-        write!(f, ", {}", generic)?;
-      }
-      write!(f, ">")?;
-    }
-    Ok(())
-  }
-}
-impl ToTokens for Type {
-  fn to_tokens(&self, stream: &mut TokenStream) {
-    if self.fully_qualified {
-      stream.append_all(quote!(::));
-    }
-    let first = &self.path[0];
-    stream.append_all(quote!(#first));
-    for part in &self.path[1..] {
-      stream.append_all(quote!(::#part));
-    }
-    if !self.generics.is_empty() {
-      stream.append_all(quote!(<));
-      stream.append_separated(self.generics.iter(), quote!(,));
-      stream.append_all(quote!(>));
-    }
-  }
-}
-
-fn gen_mapping_funcs(item_name: &Ident, args: &[Type]) -> TokenStream {
-  args
-    .iter()
-    .map(|ident| {
-      #[cfg(feature = "downcast_unchecked")]
-      let ret = quote_spanned!(ident.span() =>
-      impl ::trait_cast_rs::TraitcastableTo<dyn #ident> for #item_name {
-        fn to_dyn_ref(input: &dyn ::trait_cast_rs::TraitcastableAny) -> Option<&(dyn #ident + 'static)> {
-            let casted: &Self = unsafe { input.downcast_ref_unchecked() };
-            // SAFETY:
-            //   This is safe since we know that `input` is a instance of Self.
-            Some( casted as &dyn #ident)
-          }
-          fn to_dyn_mut(input: &mut dyn ::trait_cast_rs::TraitcastableAny) -> Option<&mut (dyn #ident + 'static)> {
-            let casted: &mut Self = unsafe { input.downcast_mut_unchecked() };
-            // SAFETY:
-            //   This is safe since we know that `input` is a instance of Self.
-            Some( casted as &mut dyn #ident)
-          }
-        }
-      );
-      #[cfg(not(feature = "downcast_unchecked"))]
-      let ret = quote_spanned!(ident.span() =>
-        impl ::trait_cast_rs::TraitcastableTo<dyn #ident> for #item_name {
-          fn to_dyn_ref(input: &dyn ::trait_cast_rs::TraitcastableAny) -> Option<&(dyn #ident + 'static)> {
-            let casted: Option<&Self> = input.downcast_ref();
-            casted.map(|selv| selv as &dyn #ident)
-          }
-          fn to_dyn_mut(input: &mut dyn ::trait_cast_rs::TraitcastableAny) -> Option<&mut (dyn #ident + 'static)> {
-            let casted: Option<&mut Self> = input.downcast_mut();
-            casted.map(|selv| selv as &mut dyn #ident)
-          }
-        }
-      );
-      ret
-    })
-    .collect::<TokenStream>()
-}
-
-fn gen_target_func(item_name: &Ident, args: &[Type]) -> TokenStream {
-  let targets = args
-    .iter()
-    .map(|ident| {
-      quote_spanned!(ident.span() =>
-        ::trait_cast_rs::TraitcastTarget::from::<#item_name, dyn #ident>(),
-      )
-    })
-    .collect::<TokenStream>();
-  let expanded = quote!(
-    impl ::trait_cast_rs::TraitcastableAny for #item_name {
-      fn traitcast_targets(&self) -> &[::trait_cast_rs::TraitcastTarget] {
-        const TARGETS: &'static [::trait_cast_rs::TraitcastTarget] = &[ #targets ];
-        TARGETS
-      }
-    }
-  );
-  expanded
-}
 /// Attribute macro implementing `TraitcastableAny` for a struct, enum or union.
 ///
 /// Use the arguments to specify all possible target Traits for witch trait objects are
@@ -261,11 +42,7 @@ fn gen_target_func(item_name: &Ident, args: &[Type]) -> TokenStream {
 /// ```
 #[proc_macro_attribute]
 pub fn make_trait_castable(args: TokenStream1, input: TokenStream1) -> TokenStream1 {
-  let args = match Type::parse_vec(TokenStream::from(args).into_iter().peekable()) {
-    Ok(args) => args,
-    Err(err) => return err.to_compile_error().into(),
-  };
-
+  let args = TokenStream::from(args);
   let input = match parse_declaration(input.into()) {
     Ok(Declaration::Function(fun)) => {
       let mut error = Error::new_at_span(
@@ -285,11 +62,9 @@ pub fn make_trait_castable(args: TokenStream1, input: TokenStream1) -> TokenStre
   };
   let item_name = input.name();
 
-  let mapping_funcs = gen_mapping_funcs(&item_name, &args);
-  let target_func = gen_target_func(&item_name, &args);
   TokenStream1::from(quote!(
     #input
-    #mapping_funcs
-    #target_func
-  ))
+    ::trait_cast_rs::make_trait_castable_decl! {
+    #item_name => (#args)
+  }))
 }
---
+++ trait_cast_rs/examples/with_decl_macro_gen.rs
@@ -0,0 +1,60 @@
+#![cfg_attr(feature = "min_specialization", feature(min_specialization))]
+#![cfg_attr(feature = "downcast_unchecked", feature(downcast_unchecked))]
+
+use std::any::type_name;
+
+use trait_cast_rs::{make_trait_castable_decl, TraitcastableAny, TraitcastableAnyInfra};
+
+extern crate trait_cast_rs;
+
+make_trait_castable_decl! {
+  HybridPet => (Dog<i32>, Dog<TestStruct<::std::primitive::i32>>, Cat<u128, u32>),
+}
+struct HybridPet {
+  name: String,
+}
+struct TestStruct<T>(T);
+impl HybridPet {
+  fn greet(&self) {
+    println!("{}: Hi", self.name)
+  }
+}
+
+impl<T> Dog<T> for HybridPet {
+  fn bark(&self) {
+    println!("{}: Woof({})!", self.name, type_name::<T>());
+  }
+}
+impl<B> Cat<u128, B> for HybridPet {
+  fn meow(&self) {
+    println!("{}: Meow(u128, {})!", self.name, type_name::<B>());
+  }
+}
+trait Dog<T> {
+  fn bark(&self);
+}
+trait Cat<A, B> {
+  fn meow(&self);
+}
+
+fn main() {
+  // The box is technically not needed but kept for added realism
+  let pet = Box::new(HybridPet {
+    name: "Kokusnuss".to_string(),
+  });
+  pet.greet();
+
+  let castable_pet: Box<dyn TraitcastableAny> = pet;
+
+  let as_dog: &dyn Dog<i32> = castable_pet.downcast_ref().unwrap();
+  as_dog.bark();
+
+  let as_dog: &dyn Dog<TestStruct<i32>> = castable_pet.downcast_ref().unwrap();
+  as_dog.bark();
+
+  let as_cat: &dyn Cat<u128, u32> = castable_pet.downcast_ref().unwrap();
+  as_cat.meow();
+
+  let cast_back: &HybridPet = castable_pet.downcast_ref().unwrap();
+  cast_back.greet();
+}
